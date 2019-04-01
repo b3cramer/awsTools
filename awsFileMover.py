6ec0146323f874112a9ba46d5765e0a767bfbd73
Matt Cramer <b3cramer@gmail.com>
2019-04-01 09:55:35 -0600

    Sketch pseudo code for  large file finder

--- awsFileMover.py
+++ awsFileMover.py
@@ -4,7 +4,14 @@ session = boto3.Session(profile_name='default')
 s3Resource = boto3.resource('s3')
 firstBucketName = 'cramerbucket1'
 secondBucketName = 'cramerbucket2'
-firstFileName = 'abc.txt'
+#firstFileName = 'abc.txt'
+
+#Get keys of objects in firstBucketName that are larger than 50MBself.
+def findLarge():
+    largeFiles = []
+    for key in value:
+        if value: u'Size' >= 50000000
+            add to largeFiles
 
 
 
@@ -15,4 +22,4 @@ def moveLarge(bucketFromName, bucketToName, fileName):
     }
     s3Resource.Object(bucketToName, fileName).copy(copySource)
 
-moveLarge(firstBucketName, secondBucketName, firstFileName)
+moveLarge(firstBucketName, secondBucketName, largeFiles)
