0337ada4a7770444c4c6998327ade06fd7428a10
Matt Cramer <b3cramer@gmail.com>
2019-04-01 13:37:51 -0600

    Modify bucket name var to take argv from command line

--- awsFileMover.py
+++ awsFileMover.py
@@ -1,17 +1,26 @@
 import boto3
+import os
+import sys
+
 
 session = boto3.Session(profile_name='default')
+#client = session.client('s3',
+#                        aws_access_key_id=os.environ.get('KEY'),
+#                        aws_secret_access_key=os.environ.get('SECRET'))
 s3Resource = boto3.resource('s3')
-firstBucketName = 'cramerbucket1'
-secondBucketName = 'cramerbucket2'
-#firstFileName = 'abc.txt'
+firstBucketName = sys.argv[1]
+secondBucketName = sys.argv[2]
+threshold = sys.argv[3]
+firstFileName = 'abc.txt'
+
+
 
 #Get keys of objects in firstBucketName that are larger than 50MBself.
-def findLarge():
-    largeFiles = []
-    for key in value:
-        if value: u'Size' >= 50000000
-            add to largeFiles
+#def findLarge():
+#    largeFiles = []
+#    for key in value:
+#        if value: u'Size' >= 50000000
+#            add to largeFiles
 
 
 
@@ -22,4 +31,4 @@ def moveLarge(bucketFromName, bucketToName, fileName):
     }
     s3Resource.Object(bucketToName, fileName).copy(copySource)
 
-moveLarge(firstBucketName, secondBucketName, largeFiles)
+moveLarge(firstBucketName, secondBucketName, firstFileName)
